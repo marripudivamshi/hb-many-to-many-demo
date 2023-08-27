f17e7272321704f67a00ccdf4080f7e36fe06816
Marripudi, Vamshi Krishna <krishna.m.dev023@gmail.com>
2023-08-27 16:52:43 -0500

    adding maven project

---
+++ src/test/java/com/example/hbmanytomanydemo/HbManyToManyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hbmanytomanydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbManyToManyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
